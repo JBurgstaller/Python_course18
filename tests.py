ed2c7b766b902e3c3d953d6f306dfbec528d67b5
burgstaller <burgstaller@neuro.mpg.de>
2018-09-25 15:27:11 +0200

    change tests to use python package for tests

--- tests.py
+++ tests.py
@@ -1,11 +1,12 @@
 from Python_Course18 import DNA
+import pytest
 
-try:
-    assert DNA('ATB')
-except ValueError:
-    pass
-assert DNA('GTC').complimentary_sequence == DNA('CAG')
-assert DNA('ATC').complimentary_sequence == DNA('TAG')
-assert DNA('ATC').complimentary_sequence == 'TAG'
 
-print('it worked!')
+def test_bad_sequence_raises_error():
+    with pytest.raises(ValueError):
+        DNA('ATB')
+
+def test_complimentary_sequence_works():
+    assert DNA('GTC').complimentary_sequence == DNA('CAG')
+    assert DNA('ATC').complimentary_sequence == DNA('TAG')
+
